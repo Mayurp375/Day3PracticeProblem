e1a452e2c277b38270a63464383d715ea08baf6c
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-04 21:27:38 +0530

    to print the elements of an array

---
+++ printElement.java
@@ -0,0 +1,14 @@
+package Day3PracticeProblem;
+
+
+import java.util.Arrays;
+
+public class printElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 34, 54, 65, 12, 25};//array of the integer
+
+        for (int e : arr) {//print using for-each loop
+            System.out.print((e)+",");
+        }
+    }
+}
