f2cc4dfefc3dac1e4887119e06b3059cd577ae1a
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-05 08:04:10 +0530

    friquency of the number is added

--- printElement.java
+++ printElement.java
@@ -1,14 +1,28 @@
 package Day3PracticeProblem;
 
 
-import java.util.Arrays;
-
 public class printElement {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 34, 54, 65, 12, 25};//array of the integer
+        int[] arr = {1, 2, 34, 34, 1, 2, 54, 65, 12, 25,13};//array of the integer
 
-        for (int e : arr) {//print using for-each loop
-            System.out.print((e)+",");
+        int visit = -1;
+        int freq[] = new int[arr.length];
+        for (int i = 0; i <= arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    freq[j] = visit;
+                }
+                if (freq[i] != visit) {
+                    freq[i] = count;
+                }
+            }
+        }
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != visit) {
+                System.out.println(arr[i] + "|" + freq[i]);
+            }
         }
     }
 }
